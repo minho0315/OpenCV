9b827987d08ab4d99d3aa9f9102fe7c2f2a7bf43
KoJaem <woalswhwh@naver.com>
2021-01-31 23:04:38 +0900

    OpenCV OMR Project CODE

---
+++ OpenCV_CODE/OpenCV_OMR_Project/OpenCV_OMR_Project/transform.hpp
@@ -0,0 +1,23 @@
+//
+//  transform.hpp
+//  OMR-BubbleSheet
+//
+//  Created by Cheling Lim on 6/2/18.
+//  Copyright 2018 Cheling Lim. All rights reserved.
+//
+
+#ifndef transform_hpp
+#define transform_hpp
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <string>
+
+using namespace cv;
+
+// Function header
+bool compareContourAreas(std::vector<Point> contour1, std::vector<Point> contour2);
+void four_point_transform(Mat& image, Mat& imageOut, std::vector<Point> docCnt);
+void sort_contour(std::vector<std::vector<Point>>& contours, int from, int to, std::string sortOrder);
+#endif /* transform_hpp */
